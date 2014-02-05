6ae4bc387fcbc0e3e2b05b87697396f78f1c205f
John Himmelman <jhimmelman@asheavenue.com>
2014-02-05 13:56:00 -0500

    Initial commit

---
+++ lib/wutbranch.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+require 'net/ssh'
+require 'optparse'
+require 'yaml'
+
+options = { }
+
+OptionParser.new do |opts|
+    opts.banner = "wutbranch"
+
+    opts.on("-g", "--group [Group name]", String, "Show branch status for servers in this group") do |v|
+        options[:group] = v
+    end
+
+    opts.on("-s", "--silent", String, "Hide messages") do |v|
+        options[:silent] = v
+    end
+end.parse!
+
+config = YAML.load(File.read(File.dirname(__FILE__) + "/../config.yaml"))
+
+servers = config['servers']
+
+if options[:group]
+    servers = servers.reject {|key, value| key['group'] != options[:group] }
+end
+
+servers.each do |server|
+    if server['is_disabled']
+        next
+    end
+
+    puts server['name']
+    puts "-----------------"
+
+    Net::SSH.start(server['host'], server['user'], :keys => [ server['key'] ]) do |ssh|
+        server['repos'].each do |name, path|
+            cmd = "cd \"#{path}\"; git rev-parse --abbrev-ref HEAD;"
+            branch = ssh.exec!(cmd)
+            branch.gsub!("\n", '').gsub!("\r", '')
+            cmd = "cd \"#{path}\"; git log --format=\"%aN @ %cr -- \\\"%s\\\"\" -n 1"
+            commit = ssh.exec!(cmd)
+            puts "#{name} on '#{branch}' last commit by #{commit}"
+        end
+    end
+
+    puts "\n"
+end
---
+++ wutbranch.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+    s.name = 'WutBranch'
+    s.version = '0.0.1'
+    s.date = '2014-02-04'
+    s.summary = "Console utility to display the current branch on remote servers."
+    s.description = "Console utility to display the current branch on remote servers."
+    s.authors = ["John Himmelman"]
+    s.email = 'john2496@gmail.com'
+    s.files = ["lib/*",
+                "bin/*",
+                "config.yaml.example"]
+    s.homepage = 'http://rubygems.org/gems/wutbranch'
+    s.license = 'MIT'
+end
